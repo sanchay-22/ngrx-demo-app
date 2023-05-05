333b0ddd7cde84b3696689889672de8728f8c671
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-05 22:37:52 +0600

    feat(post): added routing to the post list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter/counter.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'counter',
     component: CounterComponent
+  },
+  {
+    path: 'post',
+    component: PostListComponent
   }
 ];
 
