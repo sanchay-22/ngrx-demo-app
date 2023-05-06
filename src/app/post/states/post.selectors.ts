5fbbe28404b0d0684ac631fd5f6066a57c578cd3
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-06 07:22:20 +0600

    feat(post): displaying the post from state

--- src/app/post/states/post.selectors.ts
+++ src/app/post/states/post.selectors.ts
@@ -3,4 +3,4 @@ import { PostStateModel } from './post.state';
 
 const GET_POST_STATE_CONST = createFeatureSelector<PostStateModel>('posts');
 
-export const POST_SELECTOR = createSelector(GET_POST_STATE_CONST, (state) => state.posts);
+export const GET_POST_SELECTOR_CONST  = createSelector(GET_POST_STATE_CONST, (state) => state.posts);
