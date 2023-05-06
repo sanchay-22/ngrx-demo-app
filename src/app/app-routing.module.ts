b310e6a9e2d23e8ba2eab1ea94460f6c3a187500
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-06 14:23:25 +0600

    feat(post-update): created the edit form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter/counter.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { AddPostComponent } from './posts/add-post/add-post.component';
+import { EditPostComponent } from './posts/edit-post/edit-post.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
       {
         path: 'add',
         component: AddPostComponent
+      },
+      {
+        path: 'edit/:id',
+        component: EditPostComponent
       }
     ]
   }
--- src/app/posts/edit-post/edit-post.component.ts
+++ src/app/posts/edit-post/edit-post.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { PostModel } from 'src/app/shared/models/shared.model';
+import { AppStateModel } from 'src/app/shared/store/app.state';
+import { ADD_POST_ACTION_CONST } from '../states/post.action';
 
 @Component({
   selector: 'app-edit-post',
   templateUrl: './edit-post.component.html',
   styleUrls: ['./edit-post.component.css']
 })
-export class EditPostComponent {
+export class EditPostComponent implements OnInit{
+
+  editForm!: FormGroup;
+
+  constructor(private store: Store<AppStateModel>){}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.editForm = new FormGroup({
+      title: new FormControl(null,[Validators.required, Validators.minLength(5)]),
+      description: new FormControl(null, [Validators.required, Validators.minLength(5)])
+    })
+  }
+
+  editPost(): void {
+    const { title, description } = this.editForm.value;
+    const payload: PostModel = { title,description };
+
+    this.editForm.valid && this.store.dispatch(ADD_POST_ACTION_CONST({ post: payload }));
+  }
+
+  showErrors(formFieldName: string): string {
+    const formField = this.editForm.get(`${formFieldName}`);
+    const errors = formField?.errors?.['required'] ? `The ${formFieldName} is required.` : formField?.errors?.['minlength'] ? `The ${formFieldName} should be minimum of 5 characters length.` : '';
+    
+    return errors;
+  }
 
 }
