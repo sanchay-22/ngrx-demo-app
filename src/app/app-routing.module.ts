c471dd571387dba3bceac9b80229a73937e8841d
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-07 11:59:07 +0600

    feat(lazy-routing): lazy loaded counter module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   },
   {
     path: 'counter',
-    component: CounterComponent
+    loadChildren: () => import('./counter/counter.module').then(m => m.CounterModule)
   },
   {
     path: 'posts',
