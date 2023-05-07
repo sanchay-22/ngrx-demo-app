d51dcea79785395fbb1c1b7f10e09abd3646d0a6
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-07 12:14:38 +0600

    feat(lazy-loading): lazy loaded the post module

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './post-list/post-list.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PostListComponent,
+    children: [
+      {
+        path: 'add',
+        component: AddPostComponent
+      },
+      {
+        path: 'edit/:id',
+        component: EditPostComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
