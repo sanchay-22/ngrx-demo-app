2b4a71fd9f81f393858e32aee152c99acdb5289a
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-09 16:23:48 +0600

    feat(api-error): api error response handled

--- src/app/auth/components/login.component.ts
+++ src/app/auth/components/login.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { login } from '../states/auth.actions';
 import { AppStateModel } from 'src/app/shared/shared.state';
-import { setLoaderAction } from 'src/app/shared/shared.actions';
+import { setErrorMessageAction, setLoaderAction } from 'src/app/shared/shared.actions';
 import { getLoaderState } from 'src/app/shared/shared.selectors';
 
 @Component({
@@ -31,9 +31,10 @@ initializeLoginForm(): void {
   });
 }
 
-login(): void {
+login(): void { 
   this.store.dispatch(setLoaderAction({ loadingStatus: true }));//dispatching the action to set the status of loader true
   const { email, password } = this.loginForm.value;
   this.store.dispatch(login({ email, password }));//dispatching the action to login
+  this.store.dispatch(setLoaderAction({ loadingStatus: true }));//dispatching the action to set the status of loader true
 }
 }
--- src/app/shared/shared.selectors.ts
+++ src/app/shared/shared.selectors.ts
@@ -2,6 +2,7 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { ApiState } from './shared.model';
 import { StateEnum } from './shared.enum';
 
-export const LOADER_STATE_CONST = createFeatureSelector<ApiState>(StateEnum.LOADER);
+export const API_STATE_CONST = createFeatureSelector<ApiState>(StateEnum.LOADER);
 
-export const getLoaderState = createSelector(LOADER_STATE_CONST, (state) => state.loadingStatus);
+export const getLoaderState = createSelector(API_STATE_CONST, (state) => state.loadingStatus);
+export const getErrorMessageState = createSelector(API_STATE_CONST, (state) => state.errorMessage);
