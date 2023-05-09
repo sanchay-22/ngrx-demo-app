254cd43381a5a9824f5bdcaceafc0c15697b0bc7
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-09 22:29:04 +0600

    feat(logout): created the auth selector for authenticated user

---
+++ src/app/auth/states/auth.selectors.ts
@@ -0,0 +1,7 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AuthState } from './auth.state';
+import { StateEnum } from 'src/app/shared/shared.enum';
+
+const AUTH_STATE_CONST = createFeatureSelector<AuthState>(StateEnum.AUTH_STATE);
+
+export const userLoggedIn = createSelector(AUTH_STATE_CONST, state => state.user ? true : false);
