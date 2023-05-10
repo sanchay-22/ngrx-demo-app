f9a594204c1f0fbbbe533fff47991339a2febefb
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-10 11:22:03 +0600

    feat(signup): added signup form ui

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  signUpForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.initializer();
+  }
+
+  initializer(): void {
+    this.initializeSignUpForm();
+  }
+
+  initializeSignUpForm(): void {
+
+  }
+
+  onSignUp(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
