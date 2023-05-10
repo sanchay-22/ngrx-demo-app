78f999c35d086bceade79dd37f33daaaa3fd321c
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-10 11:38:18 +0600

    feat(signup): injecting the appStore to access the signup actions

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/shared/shared.state';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,6 +10,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class SignUpComponent implements OnInit {
   signUpForm!: FormGroup;
+
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.initializer();
