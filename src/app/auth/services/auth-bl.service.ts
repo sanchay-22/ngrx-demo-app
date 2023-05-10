e1564c6520848dfcb2c540fdeb22acdf0fe79c6b
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-10 17:50:21 +0600

    fix: removed unused imports

--- src/app/auth/services/auth-bl.service.ts
+++ src/app/auth/services/auth-bl.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { AuthResponseDataModel, UserModel } from '../models/auth.model';
 import { ErrorEnum, ErrorMessageEnum } from '../misc/auth.enum';
 
