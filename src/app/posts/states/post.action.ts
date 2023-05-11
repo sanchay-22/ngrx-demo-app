1f7ecb431d400c66ad66c262367d5bc55bb38917
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-11 16:23:15 +0600

    feat(post): created post actions for loading

--- src/app/posts/states/post.action.ts
+++ src/app/posts/states/post.action.ts
@@ -1,9 +1,11 @@
 import { createAction, props } from "@ngrx/store";
 import { PostModel } from 'src/app/shared/shared.model';
 
-export const POST_KEY= '[posts] :';
+export const POST_KEY= '[posts]:';
 
 export const addPostAciton = createAction(`${POST_KEY} add post`, props<{ post: PostModel}>());
 export const updatePostAction = createAction(`${POST_KEY} update post`, props<{ post: PostModel}>());
 export const deletePostAction = createAction(`${POST_KEY} delete post`, props<{ id: string }>());
 
+export const loadPostAction = createAction(`${POST_KEY} load post`);
+export const loadPostSuccessfulAction = createAction(`${POST_KEY} load post successful`, props<{ post: PostModel[] }>());
