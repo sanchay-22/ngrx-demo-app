2be412082ec603db7e606c18716c0a54b72675c6
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-11 17:14:15 +0600

    feat(post): fetched the post list from api

--- src/app/post/services/post-api.service.ts
+++ src/app/post/services/post-api.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/shared/shared.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostApiService {
+  postUrl = 'https://ngrx-testing-app-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPostList(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.postUrl);
+  }
 }
--- src/app/post/services/post-facade.service.ts
+++ src/app/post/services/post-facade.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { PostApiService } from './post-api.service';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/shared/shared.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostFacadeService {
 
-  constructor() { }
+  constructor(private postApiService: PostApiService) { }
+
+  getPostList(): Observable<Post[]> {
+    return this.postApiService.getPostList();
+  }
 }
