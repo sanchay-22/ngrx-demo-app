5ab70c4a8e1fad104d2617cdf79cc2337e3c551b
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-12 13:48:05 +0600

    feat(post): added postLoadSuccess reducer

--- src/app/post/states/post.reducer.ts
+++ src/app/post/states/post.reducer.ts
@@ -7,17 +7,15 @@ export const POST_REDUCER_CONST = createReducer(INITIAL_POST_STATE,
         ...state,
         posts: state.posts.concat({ ...action.post, id: (state.posts.length + 1).toString() })
       })),
-
     on(postActions.updatePostAction, (state, action) => ({
         ...state,
         posts: state.posts.map(post => post.id === action.post.id ? action.post : post)
       })),
-
     on(postActions.deletePostAction, (state, { id }) => ({
        ...state,
       posts: state.posts.filter(post => post.id !== id)
-    }))
-
+    })),
+    on(postActions.loadPostSuccessfulAction, (state, action) => ({ ...state, posts: action.posts })),
     );
 
 export function postReducer(state: any, action: any): any {
