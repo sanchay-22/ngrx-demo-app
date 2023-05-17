9adfb167b693753c0adce717bfe7fc7e942bbf77
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-17 12:07:51 +0600

    feat(router-state): saved and dispatching default router store actions

--- src/app/shared/shared.state.ts
+++ src/app/shared/shared.state.ts
@@ -2,13 +2,16 @@ import { AuthState, ApiState } from './shared.model';
 import { StateEnum } from './shared.enum';
 import { apiReducer } from './shared.reducer';
 import { authReducer } from '../auth/states/auth.reducers';
+import { RouterReducerState, routerReducer } from '@ngrx/router-store';
 
 export interface SharedState {
     [StateEnum.LOADER_STATE]: ApiState;
     [StateEnum.AUTH_STATE]: AuthState;
+    router: RouterReducerState
 }
 
 export const sharedReducer = {
     loaderState: apiReducer,
-    authState: authReducer
+    authState: authReducer,
+    router: routerReducer
 };
