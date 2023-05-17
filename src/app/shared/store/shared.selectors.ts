af4e46be70ddc939887a2d1d62f2cf803c2cabf3
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-17 13:09:48 +0600

    feat(router-store): added router state and curren route selector

--- src/app/shared/store/shared.selectors.ts
+++ src/app/shared/store/shared.selectors.ts
@@ -1,8 +1,11 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { ApiState } from '../misc/shared.model';
+import { ApiState, RouterStateUrl } from '../misc/shared.model';
 import { StateEnum } from '../misc/shared.enum';
+import { RouterReducerState } from '@ngrx/router-store';
 
 export const API_STATE_CONST = createFeatureSelector<ApiState>(StateEnum.LOADER_STATE);
+export const getRouterState = createFeatureSelector<RouterReducerState<RouterStateUrl>>('router sate');
 
 export const getLoaderState = createSelector(API_STATE_CONST, (state) => state.loadingStatus);
 export const getErrorMessageState = createSelector(API_STATE_CONST, (state) => state.errorMessage);
+export const getCurrentRoute = createSelector(getRouterState, (router) => router.state);
