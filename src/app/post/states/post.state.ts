666d96621fa6d09c99430e76aa82eb2ece2a5c6d
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-18 18:06:29 +0600

    fix(post): refactored post state with Entity state

--- src/app/post/states/post.state.ts
+++ src/app/post/states/post.state.ts
@@ -1,9 +1,16 @@
+import { EntityState, createEntityAdapter } from '@ngrx/entity';
 import { Post } from '../../shared/misc/shared.model';
 
-export interface PostState {
-    posts: Post[];
-}
+export interface PostState extends EntityState<Post> {} //no need to implement as we have extended
 
-export const INITIAL_POST_STATE: PostState = {
-    posts: []
-}
+export const postsAdapter = createEntityAdapter<Post>(); //creating the instance of entity adapter to simplify the crud operations on entity collection
+
+export const INITIAL_POST_STATE: PostState = postsAdapter.getInitialState(); //we dont have post intial state as we are going to get form api
+/*
+if you have post initial state then you can as below
+export const INITIAL_POST_STATE: PostState = postsAdapter.getInitialState(
+    {
+        posts: .....
+    }
+);
+*/
