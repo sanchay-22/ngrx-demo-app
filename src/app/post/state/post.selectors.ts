3aea483a8109d13669b232a3d10741c18c2286ef
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-18 19:20:48 +0600

    fix(post): fixed the getPostbyId selector

--- src/app/post/state/post.selectors.ts
+++ src/app/post/state/post.selectors.ts
@@ -1,11 +1,12 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { PostState } from './post.state';
+import { PostState, postsAdapter } from './post.state';
 import { StateEnum } from 'src/app/shared/misc/shared.enum';
 import { getCurrentRoute } from 'src/app/shared/store/shared.selectors';
 import { RouterStateUrl } from 'src/app/shared/misc/shared.model';
 
 const getPostState = createFeatureSelector<PostState>(StateEnum.POST_STATE);
 
-export const getAllPosts  = createSelector(getPostState, (state) => state.posts);
-export const getPostById = createSelector(getPostState, getCurrentRoute,
-     (posts: PostState, route: RouterStateUrl) => posts ? posts.posts.find((post) => post.id === route.params['id']) : null);
+export const postsSelectors = postsAdapter.getSelectors();
+
+export const getAllPosts  = createSelector(getPostState, postsSelectors.selectAll);
+export const getPostById = createSelector(postsSelectors.selectEntities, getCurrentRoute, (posts, route: RouterStateUrl) => posts ? posts[route.params['id']] : null)
