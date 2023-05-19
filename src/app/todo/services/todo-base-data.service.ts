bb59913c55b5f5614ae09b0fc53187b7d91dfa97
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-19 16:44:08 +0600

    feat(todos): overwitten the getall method in todoBaseDataService

--- src/app/todo/services/todo-base-data.service.ts
+++ src/app/todo/services/todo-base-data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { DefaultDataService, HttpUrlGenerator } from '@ngrx/data';
 import { Todo } from '../misc/todo.model';
 import { HttpClient } from '@angular/common/http';
+import { HttpOptions } from '@ngrx/data/src/dataservices/interfaces';
+import { Observable, map } from 'rxjs';
 
 
 @Injectable({
@@ -9,7 +11,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TodoBaseDataService extends DefaultDataService<Todo> {
 
+  todoUrl = 'https://ngrx-testing-app-default-rtdb.asia-southeast1.firebasedatabase.app/todos.json';
+
   constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator) {
     super('Todo', http, httpUrlGenerator);
+  }
+
+  override getAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.todoUrl).pipe(
+      map((data) => {
+        const todos: Todo[] = [];
+        for(let key in data) {
+          todos.push({ ...data[key], id: key });
+        }
+        return todos;
+      })
+    );
   }
 }
