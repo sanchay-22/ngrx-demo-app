d4fab8bcb94bdfc059a3178a7a71246314b6dbba
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-19 17:34:01 +0600

    fix(todo): code refactored

--- src/app/todo/services/todo-base-data.service.ts
+++ src/app/todo/services/todo-base-data.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { DefaultDataService, HttpUrlGenerator } from '@ngrx/data';
 import { Todo } from '../misc/todo.model';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { TodoBlService } from './todo-bl.service';
 
 
 @Injectable({
@@ -12,19 +13,11 @@ export class TodoBaseDataService extends DefaultDataService<Todo> {
 
   todoUrl = 'https://ngrx-testing-app-default-rtdb.asia-southeast1.firebasedatabase.app/todos.json';
 
-  constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator) {
+  constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator, private todoBlService: TodoBlService) {
     super('Todo', http, httpUrlGenerator);
   }
 
   override getAll(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todoUrl).pipe(
-      map((data) => {
-        const todos: Todo[] = [];
-        for(let key in data) {
-          todos.push({ ...data[key], id: key });
-        }
-        return todos;
-      })
-    );
+    return this.todoBlService.formatTodosResponse(this.http.get(this.todoUrl));
   }
 }
