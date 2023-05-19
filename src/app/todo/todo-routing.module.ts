99ef95e476bcf7993b79c64a851d5b4be32e56b8
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-19 13:51:37 +0600

    feat(todo): added the routing of todo

--- src/app/todo/todo-routing.module.ts
+++ src/app/todo/todo-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { CreateTodoComponent } from './components/create-todo/create-todo.component';
+import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
+import { PostDetailsComponent } from '../post/post-details/post-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoListComponent,
+    children: [
+      {
+        path: 'create',
+        component: CreateTodoComponent
+      },
+      {
+        path: 'edit/:id',
+        component: EditTodoComponent
+      },
+      {
+        path: 'detail/:id',
+        component: PostDetailsComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
