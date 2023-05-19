449e7d187ff343ce5802be1c683a018a3b3de2cd
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-19 19:02:58 +0600

    fix(todos): refactored the code in resolver

--- src/app/todo/todo.resolver.ts
+++ src/app/todo/todo.resolver.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable, first, map, mergeMap, of } from 'rxjs';
+import { Observable, first, map, mergeMap, of, switchMap, tap } from 'rxjs';
 import { TodoEntityBaseService } from './services/todo-entity-base.service';
 
 @Injectable()
@@ -9,6 +9,8 @@ export class TodosResolver implements Resolve<boolean> {
     /*
     This resolve fucntion is for route guard. It is responsible for resolving the data before activating the route. It return boolean indicating whether to activate the route or not.
      */
+
+  /*Method 1
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
         return this.todoEntityBaseService.loaded$.pipe(//    Firstly, it is accessing the loaded$ property of the TodoEntityBaseService, it is observable which returns boolean indicating whether the 'Todo' entity has been loaded or not...
             mergeMap(loaded => {//    In mergeMap if the loaded$ emits true, then the route is activated immedately, if not then it call the getAll methods of TodoEntityBaseService which will hit the api end point and fetch all the todos and returns true.
@@ -23,5 +25,13 @@ export class TodosResolver implements Resolve<boolean> {
         );
         
     }
+  */
+
+    //Method 2
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        return this.todoEntityBaseService.loaded$.pipe(
+          switchMap((loaded) => (loaded ? of(true) : this.todoEntityBaseService.getAll().pipe(map(() => true))))
+        );
+      }
 
 }
