9bb6f5987554bfa7bfa9c4db80add6eed294b3d3
Partiman Rai <partimanraia3offi@gmail.com>
2023-05-20 15:15:11 +0600

    feat(todo-update): added todo edit component

--- src/app/todo/components/edit-todo/edit-todo.component.ts
+++ src/app/todo/components/edit-todo/edit-todo.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { TodoFacadeService } from '../../services/todo-facade.service';
+import { Todo, TodoPayloadAttributes } from '../../misc/todo.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, filter, map, tap } from 'rxjs';
+import { TodoEntityBaseService } from '../../services/todo-entity-base.service';
 
 @Component({
   selector: 'app-edit-todo',
@@ -6,5 +12,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-todo.component.css']
 })
 export class EditTodoComponent {
+  editTodoForm!: FormGroup;
+  todoID!: string;
+  todo!: Todo;
+  constructor(private todoFacadeService: TodoFacadeService, private router: Router, private aRoute: ActivatedRoute, private todoEntityBaseService: TodoEntityBaseService) {}
 
+  ngOnInit(): void {
+    this.todoID = this.aRoute.snapshot.params['id'];
+    this.fetchTodo(this.todoID);
+  }
+
+  fetchTodo(todoID: string): void {
+    this.todoEntityBaseService.entities$.pipe(
+      map((todos) => todos.find(todo => todo.id === todoID)),
+      tap(todo => ((todo) ? this.initializeTodoForm(todo) : '' ))
+    ).subscribe();
+  }
+
+  initializeTodoForm(todo: Todo): void {
+    this.editTodoForm = new FormGroup({
+      title: new FormControl(todo.title),
+      des: new FormControl(todo.des),
+      status: new FormControl(todo.status),
+    });
+  }
+
+  onEditTodo(): void {
+    const payload: TodoPayloadAttributes = this.editTodoForm.value;
+    // this.todoFacadeService.addTodo(payload).subscribe(() => this.router.navigate(['/todos']));
+  }
 }
